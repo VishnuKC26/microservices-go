54e12eda66180900af3d0a445602ea3dd3954498
VishnuKC26 <vishnukantchaudhary@gmail.com>
2026-07-07 12:07:24 +0530

    configured websockets

--- shared/types/types.go
+++ shared/types/types.go
@@ -19,7 +19,7 @@ type OsrmApiResponse struct {
 		Distance float64 `json:"distance"`
 		Duration float64 `json:"duration"`
 		Geometry struct {
-			Coordinates [][]float64 `json:"cooridnates"`
+			Coordinates [][]float64 `json:"coordinates"`
 		} `json:"geometry"`
 	} `json:"routes"`
 }
